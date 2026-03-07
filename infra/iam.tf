07ad56d274dc3211a4854c4bcd8a0fa5d58ed73b
RithikaMurugannn <148222233+RithikaMurugannn@users.noreply.github.com>
2026-03-07 01:58:30 -0800

    Update iam.tf

---
+++ infra/iam.tf
@@ -0,0 +1,32 @@
+variable "pub_instance_ami "{
+description = "eip name"
+}
+
+variable " pub_instance_type"{
+description = "eip name"
+}
+
+variable "associate_public_ip_address "{
+description = "eip name"
+}
+
+variable "pub_instance_name"{
+description = "name"
+}
+
+
+variable "prvt_instance_ami "{
+description = "eip name"
+}
+
+variable " prvt_instance_type"{
+description = "eip name"
+}
+
+variable "associate_public_ip_address "{
+description = "eip name"
+}
+
+variable "prvt_instance_name"{
+description = "name"
+}
