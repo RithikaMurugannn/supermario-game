4c34e19a41cd27e48dbae2a5b0ecea3d308fdf09
RithikaMurugannn <148222233+RithikaMurugannn@users.noreply.github.com>
2026-03-07 01:57:25 -0800

    Update vpc.tf

---
+++ infra/vpc.tf
@@ -0,0 +1,11 @@
+variable "vpc_cidr_blcok" {
+  description = "this is for vpc cidr block"
+}
+
+variable "instance_tenancy" {
+  description = "this is vpc tenancy name"
+}
+
+variable "vpc_name" {
+  description = "this is a vpc name"
+}
