3c2353fc272515b61243f0b4d8b04679e8e6ea05
RithikaMurugannn <148222233+RithikaMurugannn@users.noreply.github.com>
2026-03-08 07:18:32 +0530

    Update eks.tf

--- infra/eks.tf
+++ infra/eks.tf
@@ -1,7 +1,20 @@
-terraform {
-  backend "s3" {
-    bucket = "mario12bucket" # Replace with your actual S3 bucket name
-    key    = "EKS/terraform.tfstate"
-    region = "us-east-1"
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.0"
+
+  cluster_name    = var.cluster_name
+  cluster_version = "1.29"
+
+  subnet_ids = module.vpc.private_subnets
+  vpc_id     = module.vpc.vpc_id
+
+  eks_managed_node_groups = {
+    default = {
+      instance_types = ["t3.medium"]
+
+      min_size     = 1
+      max_size     = 2
+      desired_size = 2
+    }
   }
 }
