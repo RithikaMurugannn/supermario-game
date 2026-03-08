5b083e35c20c4c9db16e26770c3c794b5880a80e
RithikaMurugannn <148222233+RithikaMurugannn@users.noreply.github.com>
2026-03-08 07:20:43 +0530

    Update build_and_push.sh

--- scripts/build_and_push.sh
+++ scripts/build_and_push.sh
@@ -1,3 +1,16 @@
+#!/bin/bash
+
+ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
+REGION=eu-west-1
+REPO=supermario
+
+aws ecr create-repository --repository-name $REPO || true
+
+aws ecr get-login-password --region eu-west-1 \
+| docker login --username AWS \
+--password-stdin rithika17.dkr.ecr.eu-west-1.amazonaws.com
+
 docker build -t mario:latest
 docker tag mario rithika17/mario:latest
 docker push rithika17/mario:latest
+
